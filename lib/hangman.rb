9b6b59433c60f62de067a68972b1556d9a632b1a
Franklin <Franklinau2@gmail.com>
2022-08-14 11:04:08 -0700

    Add player guessing and check if guess is valid

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -7,7 +7,8 @@ module Hangman
     end
 
     def guess_letter
-      
+      puts "Input your guess: "
+      guess = gets.chomp.downcase
     end
   end
 
@@ -21,12 +22,24 @@ module Hangman
       answer = select_random_word
       p answer # used for testing, get rid after
       display = Array.new(answer.length, '_')
+      letters_guessed = Array.new
       p display
       guesses = 0
       while guesses < 6
         # display the display first
         print_array(display)
-        player_guess = @player.guess_letter
+        # print out which letters have been guessed
+        print_array(letters_guessed)
+        loop do
+          player_guess = @player.guess_letter
+          p player_guess.length
+          if valid_guess?(player_guess, letters_guessed)
+            letters_guessed.push(player_guess)
+            break
+          end
+          puts "Sorry, that was an invalid input."
+          puts "Please type in a letter that has not been guessed before."
+        end
         # check the letter in the guess is in the array
         # break if word is guessed correctly
         guesses += 1
@@ -62,8 +75,12 @@ module Hangman
     end
 
 
-    def check_guess
-
+    def valid_guess?(guess, letters_guessed)
+      # false if guess.length != 1
+      return false if guess.length != 1
+      return false unless guess.match?(/[[:alpha:]]/)
+      return false if letters_guessed.include? guess
+      true
     end
   end
 end
